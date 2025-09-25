10eef2a7b893339219a916ee8db1523fba79fb25
VK4041 <151887513+VK4041@users.noreply.github.com>
2025-09-25 12:02:30 +1000

    Add console gameplay logic
    win-con #1 only horizontal rows

--- script.js
+++ script.js
@@ -1,39 +1,87 @@
 const Gameboard = {
-    gameboard: [],
-    createGameBoard() {
-        for (let i = 0; i < 3; i++) {
-            this.gameboard.push([])
-            for (let j = 0; j < 3; j++) {
-                this.gameboard[i].push('')
-            }
-        }
-    },
-    getGameBoard() {
-        return this.gameboard
+  gameboard: [],
+  initGameboard() {
+    for (let i = 0; i < 3; i++) {
+      this.gameboard.push([]);
+      for (let j = 0; j < 3; j++) {
+        this.gameboard[i].push("");
+      }
     }
-}
+  },
+  getGameBoard() {
+    return this.gameboard;
+  },
+  populateBoard(player, index) {
+    this.getGameBoard()[index.row][index.col] = player.choice;
+    console.log(this.getGameBoard()[index.row][index.col]);
+  },
+};
 const Players = {
-    player1: { choice: 'O', name: 'Player1' },
-    playerCpu: { choice: 'X', name: 'CPU' }
-}
+  player1: { choice: "O", name: "Player1" },
+  player2: { choice: "X", name: "Player2" },
+};
 const Controller = {
-    displayController: function () {
-        Gameboard.createGameBoard()
-        Gameboard.getGameBoard()
-        console.log(this.playerTurn)
-        let row = prompt('Enter row index')
-        let col = prompt('Enter col index')
-        //let playerTurn be player1 for now
-        
-        this.populateBoard(this.playerTurn, row, col)
-        console.log(Gameboard.getGameBoard())
-    },
-    playerTurn: Players.player1,
-    populateBoard(player, row = 1, col = 1) {
-        Gameboard.getGameBoard()[row][col] = player.choice
-        console.log(Gameboard.getGameBoard()[row][col])
-
-        // if (Gameboard.getGameBoard()[row][col])
+  playerTurn: Players.player1,
+  displayController() {
+    Gameboard.initGameboard();
+    this.playGame();
+  },
+  playGame() {
+    while (!this.winCondition()) {
+      this.turnToggler();
     }
-}
-Controller.displayController()
+    this.displayWinner();
+  },
+  displayWinner() {
+    console.log(
+      `Winner is ${this.playerTurn.name} (${this.playerTurn.choice})`
+    );
+  },
+  turnToggler() {
+    if (this.playerTurn === Players.player1) {
+      this.getPlayer1Choice();
+      this.playerTurn = Players.player2;
+    } else {
+      this.getPlayer2Choice();
+      this.playerTurn = Players.player1;
+    }
+  },
+  getPlayer1Choice() {
+    let row;
+    let col;
+    do {
+      row = prompt("Enter row index", 1);
+      col = prompt("Enter col index", 1);
+    } while (this.cellOccupied(row, col));
+
+    Gameboard.populateBoard(this.playerTurn, { row, col });
+    console.log("Player1");
+    console.table(Gameboard.getGameBoard());
+  },
+  getPlayer2Choice() {
+    let row;
+    let col;
+    //Create random index s.t. they are not occupied already
+    do {
+      row = Math.floor(Math.random() * 3);
+      col = Math.floor(Math.random() * 3);
+    } while (this.cellOccupied(row, col));
+    let player2Choice = { row, col };
+    this.winCondition(this.playerTurn);
+    Gameboard.populateBoard(this.playerTurn, player2Choice);
+    console.log("Player2");
+    console.table(Gameboard.getGameBoard());
+  },
+  winCondition() {
+    //each row check
+    let mark = this.playerTurn.choice;
+    let rowCheck = Gameboard.getGameBoard().map((row) =>
+      row.every((cell) => cell === mark)
+    );
+    return rowCheck.some((result) => result);
+  },
+  cellOccupied(row, col) {
+    return !!Gameboard.getGameBoard()[row][col];
+  },
+};
+Controller.displayController();
