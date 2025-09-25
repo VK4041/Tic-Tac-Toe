4a3f2b62d62c40f9addc85fea2682ae87330904a
VK4041 <151887513+VK4041@users.noreply.github.com>
2025-09-25 19:08:46 +1000

    Complete console logic
    Add all win-conditions
    Add draw logic
    Optimise search through roundCount variable check

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const Gameboard = {
       }
     }
   },
+  resetGameboard() {
+    this.gameboard = [];
+  },
   getGameBoard() {
     return this.gameboard;
   },
@@ -17,48 +20,62 @@ const Gameboard = {
   },
 };
 const Players = {
-  player1: { choice: "O", name: "Player1" },
-  player2: { choice: "X", name: "Player2" },
+  player1: { choice: "O", name: "Player1", score: 0 },
+  player2: { choice: "X", name: "Player2", score: 0 },
 };
 const Controller = {
   playerTurn: Players.player1,
-  displayController() {
+  roundCount: 0,
+  startGame() {
     console.clear();
     Gameboard.initGameboard();
     this.playGame();
   },
+  //   displayController() {
+  //     //Use for DOM content
+  //   },
   playGame() {
-    while (!this.winCondition()) {
+    while (this.roundCount < 3 || !this.winCondition()) {
+      if (this.checkDraw()) break;
       this.turnToggler();
     }
     this.displayWinner();
   },
+  checkDraw() {
+    return this.roundCount === 5;
+  },
   displayWinner() {
+    if (this.checkDraw()) {
+      console.log("DRAW");
+    } else {
+      this.playerTurn.score++;
+      console.log(
+        `Game-Over \nThe Winner is ${this.playerTurn.name} (${this.playerTurn.choice})`
+      );
+    }
     console.log(
-      `Winner is ${this.playerTurn.name} (${this.playerTurn.choice})\nGame-Over`
+      `Scoreboard\n ${Players.player1.name} = ${Players.player1.score} |
+       ${Players.player2.name} = ${Players.player2.score}`
     );
+    //Reset game
+    this.playerTurn = Players.player1;
+    this.roundCount = 0;
+    Gameboard.resetGameboard();
   },
   turnToggler() {
     if (this.playerTurn === Players.player1) {
       this.getPlayer1Choice();
+      if (this.roundCount >= 3 && this.winCondition()) {
+        return;
+      }
       this.playerTurn = Players.player2;
     } else {
       this.getPlayer2Choice();
+      if (this.roundCount >= 3 && this.winCondition()) {
+        return;
+      }
       this.playerTurn = Players.player1;
     }
-  },
-  getPlayer1Choice() {
-    let row;
-    let col;
-    do {
-      row = parseInt(prompt("Enter row index [0, 1 or 2]", 1));
-      col = parseInt(prompt("Enter col index [0, 1 or 2]", 1));
-      if (!row || !col) break;
-    } while (this.isInValidIndex(row, col) || this.cellOccupied(row, col));
-
-    Gameboard.populateBoard(this.playerTurn, { row, col });
-    console.log("Player1");
-    console.table(Gameboard.getGameBoard());
   },
   isInValidIndex(row, col) {
     return (
@@ -70,6 +87,19 @@ const Controller = {
       col > 2
     );
   },
+  getPlayer1Choice() {
+    let row;
+    let col;
+    do {
+      row = parseInt(prompt("Enter row index [0, 1 or 2]", 1));
+      col = parseInt(prompt("Enter col index [0, 1 or 2]", 1));
+      if (!row || !col) break;
+    } while (this.isInValidIndex(row, col) || this.cellOccupied(row, col));
+    Gameboard.populateBoard(this.playerTurn, { row, col });
+    console.log("Player1");
+    console.table(Gameboard.getGameBoard());
+    this.roundCount++;
+  },
   getPlayer2Choice() {
     let row;
     let col;
@@ -79,21 +109,52 @@ const Controller = {
       col = Math.floor(Math.random() * 3);
     } while (this.cellOccupied(row, col));
     let player2Choice = { row, col };
-    this.winCondition(this.playerTurn);
     Gameboard.populateBoard(this.playerTurn, player2Choice);
     console.log("Player2");
     console.table(Gameboard.getGameBoard());
   },
   winCondition() {
     //each row check
+    let win = false;
     let mark = this.playerTurn.choice;
-    let rowCheck = Gameboard.getGameBoard().map((row) =>
+    let horWin = Gameboard.getGameBoard().map((row) =>
       row.every((cell) => cell === mark)
     );
-    return rowCheck.some((result) => result);
+    win = horWin.some((result) => result);
+    //return early if already won
+    if (win) return win;
+
+    //each column check
+    let columns = [[], [], []];
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        columns[i].push(Gameboard.getGameBoard()[j][i]);
+      }
+    }
+    let vertWin = columns.map((col) => col.every((cell) => cell === mark));
+    win = vertWin.some((result) => result);
+    //return early if already won
+    if (win) return win;
+
+    //each diagonal check
+    let diagonals = [[], []];
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (i === j) {
+          diagonals[0].push(Gameboard.getGameBoard()[i][j]);
+        }
+        if (i + j === 2) {
+          diagonals[1].push(Gameboard.getGameBoard()[i][j]);
+        }
+      }
+    }
+    let diagWin = diagonals.map((diag) => diag.every((cell) => cell === mark));
+    win = diagWin.some((result) => result);
+
+    //return regardless of win
+    return win;
   },
   cellOccupied(row, col) {
     return !!Gameboard.getGameBoard()[row][col];
   },
 };
-Controller.displayController();
